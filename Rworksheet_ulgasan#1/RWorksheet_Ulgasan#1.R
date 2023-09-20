c069d2a804092c49001ee2159ec9445838cdc629
Jhofer John Ulgasan <jhoferjohn.ulgasan@gmail.com>
2023-09-20 04:07:33 +0000

    int commit answered until number 8.5

---
+++ Rworksheet_ulgasan#1/RWorksheet_Ulgasan#1.R
@@ -0,0 +1,81 @@
+
+age <-c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29,
+          35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41,
+          51, 35, 24, 33, 41)
+age
+
+
+length(age)
+
+reciprocalAge <- 1 / age
+reciprocalAge
+
+new_age <- c(age, 0, age)
+new_age
+
+sortAge  <-sort(age)
+sortAge
+
+minimumAge <-min(age)
+minimumAge
+
+maximumAge <-max(age)
+maximumAge
+
+
+data <-c (2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3,
+        2.5, 2.3, 2.4, 2.7)
+
+data
+
+length(data)
+
+new_data <- data *2
+new_data
+
+int1To100 <- seq(1,100)
+int1To100
+
+numbers20To60 <- seq(20,60)
+numbers20To60
+
+meanofnum <- mean(20,60)
+meanofnum
+
+sumofnum <- sum(51,91)
+sumofnum
+
+int1to1000 <- seq(1,1000)
+int1to1000
+
+lengthofnewdata <- length(new_data)
+lengthofnewdata
+
+lengthofint100 <- length(int1To100)
+lengthofint100
+lengthnum20 <- length(numbers20To60)
+lengthnum20
+lengthmean <- length(meanofnum)
+lengthmean
+lengthsum <- length(sumofnum)
+lengthint1000 <- length(int1to1000)
+
+
+allDataPoints <- lengthofnewdata + lengthofint100 + lengthnum20 + lengthmean + lengthsum + lengthint1000
+allDataPoints
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+           
